53e644bcc59e31cde654f3e33e7e8b83cc0870ec
jasmine0714 <woooooooow22@gmail.com>
2024-02-21 17:56:13 +0900

    ⚡️ Logo에 home으로 redirect하는 Link tag 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,12 @@ import React from 'react';
 const Header = () => {
 	return (
 		<header className="flex flex-row items-center justify-around h-16 border-b border-gray-light">
-			<section className="flex justify-center w-1/6 m-4 text-gray text-nowrap">
+			<Link
+				href={NavItems.home}
+				className="flex justify-center w-1/6 m-4 text-gray text-nowrap"
+			>
 				Tech Blog_Jasmine
-			</section>
+			</Link>
 			<nav>
 				<ul className="flex flex-row">
 					{Object.entries(NavItems).map(([label, href]) => (
