2118506e43a28d1b8321ca57af95a8162e9ee824
jasmine0714 <woooooooow22@gmail.com>
2024-02-21 21:56:49 +0900

    💄 Image 컴포넌트 사용하여 프로필 사진 화면에 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import Image from 'next/image';
+import profileImage from '../../public/profile.jpeg';
+
 export default async function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1 className="text-black px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
-				tech blog
-			</h1>
+		<main className="flex h-screen flex-col items-center justify-between p-24">
+			<section className="flex flex-col items-center justify-center">
+				<div>
+					<Image
+						alt="profile image"
+						src={profileImage}
+						width={400}
+						height={400}
+						className="border border-gray-light rounded-full object-cover h-80 w-80 "
+					/>
+				</div>
+				<div>
+					<h1>Lee Minsun</h1>
+				</div>
+			</section>
 		</main>
 	);
 }
