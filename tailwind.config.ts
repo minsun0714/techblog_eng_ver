e9410d7c7f4e6de561241ff1161476ede6c95b69
jasmine0714 <woooooooow22@gmail.com>
2024-02-22 17:35:32 +0900

    🔧 color palette에 gray-mid-light 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    colors: {
-      black: "#000",
-      gray: "#273444",
-      "gray-light": "#d3dce6",
-      white: "#fff",
-    },
-    extend: {
-      spacing: {
-        "128": "32rem",
-        "144": "36rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-    },
-  },
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		colors: {
+			black: '#000',
+			gray: '#273444',
+			'gray-mid-light': '#64748b',
+			'gray-light': '#d3dce6',
+			white: '#fff',
+		},
+		extend: {
+			spacing: {
+				'128': '32rem',
+				'144': '36rem',
+			},
+			borderRadius: {
+				'4xl': '2rem',
+			},
+		},
+	},
 };
 export default config;
