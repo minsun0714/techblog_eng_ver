0aa9b95d0a3ad00c0dcc48e8dc355fe9b6165943
jasmine0714 <woooooooow22@gmail.com>
2024-02-22 17:37:40 +0900

    🎨 social link를 enum으로 관리할 수 있도록 별도 파일에 추가

--- src/util/enum.ts
+++ src/util/enum.ts
@@ -2,3 +2,8 @@ export enum NavItems {
 	'home' = '/',
 	'posts' = '/posts',
 }
+
+export enum SocialLink {
+	Github = 'https://github.com/minsun0714',
+	Velog = 'https://velog.io/@jasmine0714/posts',
+}
