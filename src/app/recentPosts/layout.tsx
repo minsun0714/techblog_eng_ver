1e81e0b2a65180e72966c0b38ecdebc3f8bc8ea6
jasmine0714 <woooooooow22@gmail.com>
2024-02-23 13:18:46 +0900

    ✨ 중첩 layout 사용하여 최근 게시물 영역 생성

---
+++ src/app/recentPosts/layout.tsx
@@ -0,0 +1,4 @@
+function RecentPostsLayout({ children }: { children: React.ReactNode }) {
+	return <section>{children}</section>;
+}
+export default RecentPostsLayout;
---
+++ src/app/recentPosts/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecentPostsPage = () => {
+	return <div>Recent Posts</div>;
+};
+
+export default RecentPostsPage;
