707118f7fe31383bea7bb4d22f4af1631bcb001a
jasmine0714 <woooooooow22@gmail.com>
2024-02-23 13:56:32 +0900

    🤡 dummy card를 활용하여 최근 게시물 영역에 grid 3열 적용

--- src/app/recentPosts/page.tsx
+++ src/app/recentPosts/page.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Card from '@/components/Card';
+import DummyImage from 'public/dummyImg.png';
+
+const DummyCards = () => {
+	return (
+		<li className="flex flex-col items-center justify-center">
+			<Card imgUrl={DummyImage} title="card" date="2023.02.23" />
+		</li>
+	);
+};
 
 const RecentPostsPage = () => {
-	return <div>Recent Posts</div>;
+	const array = [...Array(6)];
+	return (
+		<div>
+			<h1 className="p-2">Recent Posts</h1>
+			<ul className="grid grid-cols-3 items-center justify-center gap-10">
+				{array.map((_, index) => (
+					<DummyCards key={index} />
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default RecentPostsPage;
