32f3888ec70857185da2a162fb76db3852f6c64e
jasmine0714 <woooooooow22@gmail.com>
2024-02-23 13:55:23 +0900

    💄 최근 게시물 layout에 height, width 등 스타일 적용

--- src/app/recentPosts/layout.tsx
+++ src/app/recentPosts/layout.tsx
@@ -1,4 +1,4 @@
 function RecentPostsLayout({ children }: { children: React.ReactNode }) {
-	return <section>{children}</section>;
+	return <section className="h-screen w-full p-32">{children}</section>;
 }
 export default RecentPostsLayout;
