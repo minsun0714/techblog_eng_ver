5e7ab28ffc45bc7a79fd98614fbb7c904f92bbe7
jasmine0714 <woooooooow22@gmail.com>
2024-02-24 17:23:53 +0900

    💄 카드 컴포넌트 페이지별 크기에 따른 조건부 스타일링

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,27 +2,58 @@ import Image, { StaticImageData } from 'next/image';
 import React from 'react';
 
 type CardProps = {
+	isBig?: boolean;
 	imgUrl: StaticImageData;
 	title: string;
-	date: string;
 };
 
-const Card = ({ imgUrl, title, date }: CardProps) => {
+const bigCardStyle = {
+	height: 'h-[180px]',
+	width: 'w-1000px',
+	minWidth: 'min-w-[1002px]',
+	bottomHeight: 'h-40',
+	bottomMinWidth: 'w-[1000px]',
+};
+
+const smallCardStyle = {
+	height: 'h-[220px]',
+	width: 'w-380px',
+	minWidth: 'min-w-[382px]',
+	bottomHeight: 'h-24',
+	bottomMinWidth: 'w-[380px]',
+};
+
+const Card = ({ isBig, imgUrl, title }: CardProps) => {
 	return (
-		<div className="relative flex h-[220px] min-w-[382px] flex-col justify-end">
+		<div
+			className={
+				'relative flex' +
+				` ${isBig ? bigCardStyle.height : smallCardStyle.height} ${isBig ? bigCardStyle.minWidth : smallCardStyle.minWidth} ` +
+				'flex-col justify-end'
+			}
+		>
 			<Image
 				className="absolute my-3 rounded-2xl object-cover"
 				src={imgUrl}
 				alt={imgUrl.src.slice(imgUrl.src.lastIndexOf('/') + 1)}
 			/>
 			<div className="absolute rounded-b-2xl border border-gray-light bg-white ">
-				<div className="relative flex h-24 min-w-[380px] flex-col items-center justify-stretch pt-3">
-					<label className="absolute w-[430px] pl-10">
+				<div
+					className={
+						'relative flex' +
+						` ${isBig ? bigCardStyle.bottomHeight : smallCardStyle.bottomHeight} ${isBig ? bigCardStyle.bottomMinWidth : smallCardStyle.bottomMinWidth} ` +
+						'flex-col items-center justify-stretch pt-3'
+					}
+				>
+					<label
+						className={
+							'absolute' +
+							` ${isBig ? bigCardStyle.minWidth : smallCardStyle.minWidth} ` +
+							'pl-10'
+						}
+					>
 						{title.length <= 60 ? title : title.slice(0, 60) + '..'}
 					</label>
-					<time className="absolute mt-12 h-4 w-[380px] pr-6 text-end text-gray-light">
-						{date}
-					</time>
 				</div>
 			</div>
 		</div>
