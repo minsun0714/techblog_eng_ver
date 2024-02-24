980b964f934d93c1ad8664a40a0a665af84fbd76
jasmine0714 <woooooooow22@gmail.com>
2024-02-24 12:18:18 +0900

    ✨ sidebar에 menu data 렌더링

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,9 +1,39 @@
-function RecentPostsLayout({ children }: { children: React.ReactNode }) {
+import getSideMenuData from '@/service/fetchMenu';
+
+const RecentPostsLayout = async ({
+	children,
+}: {
+	children: React.ReactNode;
+}) => {
+	let menuData = await getSideMenuData();
+	menuData = Object.entries(menuData);
+
 	return (
 		<section className="flex flex-row">
-			<aside className="h-screen w-96 bg-black text-white">side menu</aside>
+			<aside className="flex h-screen w-96 flex-col items-start justify-start bg-black text-gray-light">
+				<ul className="m-16">
+					{menuData.map(
+						([menu, subMenuList]: [string, string[]], i: number) => {
+							return (
+								<li key={i} className="my-4">
+									{'❍ ' + menu}
+									<ul>
+										{subMenuList.map((subMenu, j) => {
+											return (
+												<li className="my-1 ml-4" key={j}>
+													{'❑ ' + subMenu}
+												</li>
+											);
+										})}
+									</ul>
+								</li>
+							);
+						},
+					)}
+				</ul>
+			</aside>
 			{children}
 		</section>
 	);
-}
+};
 export default RecentPostsLayout;
