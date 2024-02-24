a3d75010863e44212c7c032e9562d7be97676b43
jasmine0714 <woooooooow22@gmail.com>
2024-02-24 10:41:20 +0900

    💄 column이 2개 이하일 때도 padding-bottom이 적용되도록 변경

--- src/app/recentPosts/page.tsx
+++ src/app/recentPosts/page.tsx
@@ -13,7 +13,7 @@ const DummyCards = () => {
 const RecentPostsPage = () => {
 	const array = [...Array(6)];
 	return (
-		<ul className="mt-16 grid grid-cols-1 gap-x-4 gap-y-28 lg:grid-cols-2 xl:grid-cols-3">
+		<ul className="grid grid-cols-1 gap-x-4 gap-y-28 py-12 lg:grid-cols-2 xl:grid-cols-3">
 			{array.map((_, index) => (
 				<DummyCards key={index} />
 			))}
