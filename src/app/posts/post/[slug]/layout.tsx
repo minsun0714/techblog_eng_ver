cda1f3868b6c0c602f9ffe4966ec81e1ba7cf645
jasmine0714 <woooooooow22@gmail.com>
2024-02-27 17:57:31 +0900

    ✅ 게시물 내부 flex column 적용

--- src/app/posts/post/[slug]/layout.tsx
+++ src/app/posts/post/[slug]/layout.tsx
@@ -1,6 +1,6 @@
 function PostLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<section className="m-40 flex h-full w-2/3 text-pretty border border-gray p-10">
+		<section className="m-40 flex h-full w-2/3 flex-col border border-gray p-10">
 			{children}
 		</section>
 	);
