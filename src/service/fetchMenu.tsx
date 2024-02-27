cefbdd64853a6ecbd28d5f94a82e65ecb4f480ec
jasmine0714 <woooooooow22@gmail.com>
2024-02-27 20:06:27 +0900

    🚨 json 변수에 아무런 값도 할당되지 못하는 경우 타입 가드 적용

--- src/service/fetchMenu.tsx
+++ src/service/fetchMenu.tsx
@@ -1,4 +1,4 @@
-const { promises } = require('fs');
+import { promises } from 'fs';
 
 type MenuData = {
 	[key: string]: string[];
@@ -8,6 +8,11 @@ const getSideMenuData = async (): Promise<MenuData> => {
 	const json = await promises
 		.readFile('menu.json', 'utf-8')
 		.catch(console.error);
+
+	if (!json) {
+		throw new Error('메뉴 데이터를 찾는 과정에서 에러가 발생했습니다.');
+	}
+
 	const menuData = await JSON.parse(json);
 	return menuData;
 };
