b49b7c381ade62ffdd974fce134c3dcea0bfb073
jasmine0714 <woooooooow22@gmail.com>
2024-02-27 18:52:56 +0900

    ✨ sidebar에 새롭게 생성한 path로 Link tag 적용

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 export type MenuDataTuple = [string, string[]];
@@ -11,14 +12,18 @@ const SideBar = ({ menuData }: SideBarProps) => {
 		<aside className="sticky top-0 hidden h-screen flex-col whitespace-nowrap bg-black text-gray-light xl:flex xl:min-w-[400px] xl:justify-start">
 			<ul className="mx-16 my-24">
 				{menuData.map(([menu, subMenuList]: MenuDataTuple, i: number) => {
+					const menuPath = menu.toLowerCase();
 					return (
 						<li key={i} className="my-4">
-							{'❍ ' + menu}
+							<Link href={`/posts/${menuPath}`}>{'❍ ' + menu}</Link>
 							<ul>
 								{subMenuList.map((subMenu, j) => {
+									const subMenuPath = subMenu.toLowerCase().replace(/ /g, '-');
 									return (
 										<li className="my-1 ml-4" key={j}>
-											{'❑ ' + subMenu}
+											<Link href={`/posts/${menuPath}/${subMenuPath}`}>
+												{'❑ ' + subMenu}
+											</Link>
 										</li>
 									);
 								})}
