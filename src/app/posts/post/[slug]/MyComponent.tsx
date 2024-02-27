c0cd2f105a45489c1c14f8e9942a3a1f69ac5691
jasmine0714 <woooooooow22@gmail.com>
2024-02-27 17:26:04 +0900

    ✨ MDX 예시 페이지 생성

---
+++ src/app/posts/post/[slug]/MyComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponent = () => {
+	return (
+		<button className="h-20 w-20 rounded-lg bg-gray text-gray-light">
+			button
+		</button>
+	);
+};
+
+export default MyComponent;
