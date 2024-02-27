6364a73eb64557176a591654c24fa3721ae9c72a
jasmine0714 <woooooooow22@gmail.com>
2024-02-27 17:12:46 +0900

    🔧 MDX 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
-/** @type {import('next').NextConfig} */
+import nextMDX from '@next/mdx';
+
+const withMDX = nextMDX({
+	extension: /\.mdx?$/,
+});
+
 const nextConfig = {
 	experimental: {
 		typedRoutes: true,
 	},
+	pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
