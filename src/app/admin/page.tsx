8e823774822efb68ee63fa59469ea474677a0887
jasmine0714 <woooooooow22@gmail.com>
2024-02-28 16:07:48 +0900

    ✨ 상태와 무관히 Input 내부의 값이 유지되도록 react-hook-form 적용

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,21 +1,36 @@
 'use client';
 
 import React, { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { GoChevronUp } from 'react-icons/go';
 import { GoChevronDown } from 'react-icons/go';
 import MDEditor from '@uiw/react-md-editor';
 
+type FormElements = {
+	title: string;
+	category1: string;
+	category2: string;
+};
+
 const AdminPage = () => {
-	const [value, setValue] = useState<string | undefined>('');
 	const [isOpen, setIsOpen] = useState<boolean>(true);
+	const [value, setValue] = useState<string | undefined>('');
+
+	const { register, handleSubmit } = useForm<FormElements>();
+
+	const onSubmit: SubmitHandler<FormElements> = (data) => console.log(data);
 
 	return (
-		<form className="flex w-2/3 flex-col gap-y-8 p-20">
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className="flex w-2/3 flex-col gap-y-8 p-20"
+		>
 			{isOpen && (
 				<>
 					<div>
 						<label className="text-gray-mid-light">title</label>
 						<input
+							{...register('title', { required: true })}
 							className="h-10 w-full rounded-sm border border-gray-mid-light p-3
 			focus:outline-gray
 			"
@@ -24,11 +39,17 @@ const AdminPage = () => {
 					<ul className="flex w-full flex-row gap-x-12 focus:outline-gray">
 						<li className="flex w-full flex-col">
 							<label className="text-gray-mid-light">category 1</label>
-							<input className="h-10 rounded-sm border border-gray-mid-light focus:outline-gray" />
+							<input
+								{...register('category1', { required: true })}
+								className="h-10 rounded-sm border border-gray-mid-light focus:outline-gray"
+							/>
 						</li>
 						<li className="flex w-full flex-col">
 							<label className="text-gray-mid-light">category 2</label>
-							<input className="h-10 rounded-sm border border-gray-mid-light focus:outline-gray" />
+							<input
+								{...register('category2', { required: true })}
+								className="h-10 rounded-sm border border-gray-mid-light focus:outline-gray"
+							/>
 						</li>
 					</ul>
 					<MDEditor value={value} onChange={setValue} />
@@ -48,10 +69,7 @@ const AdminPage = () => {
 				style={{ overflowY: 'scroll', height: '85vh' }}
 			/>
 			<div className="flex justify-center">
-				<button
-					type="button"
-					className="h-16 w-60 rounded-md bg-gray text-white"
-				>
+				<button className="h-16 w-60 rounded-md bg-gray text-white">
 					submit
 				</button>
 			</div>
