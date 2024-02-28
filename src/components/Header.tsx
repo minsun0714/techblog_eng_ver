ec1b921d4962cb3e5f7d40cd2d38cdeb3d11d44d
jasmine0714 <woooooooow22@gmail.com>
2024-02-28 13:40:36 +0900

    💄 header fixed에서 sticky로 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { NavItems } from '@/util/enum';
 
 const Header = () => {
 	return (
-		<header className="fixed z-50 flex h-16 w-screen flex-row items-center justify-around border-b border-gray-light bg-white">
+		<header className="sticky z-50 flex h-16 w-screen flex-row items-center justify-around border-b border-gray-light bg-white">
 			<Link
 				href={NavItems.home}
 				className="m-4 flex w-1/6 justify-center text-nowrap text-gray"
