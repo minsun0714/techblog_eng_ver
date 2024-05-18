b69beedd7ca4eda316938ea40962dd0065c860e7
jasmine0714 <woooooooow22@gmail.com>
2024-05-18 14:09:53 +0900

    💄 Header에 fixed 적용

--- src/app/(common)/components/Header.tsx
+++ src/app/(common)/components/Header.tsx
@@ -4,23 +4,25 @@ import Link from 'next/link';
 
 const Header = () => {
 	return (
-		<header className="sticky z-50 flex h-16 w-screen flex-row items-center justify-around border-b border-gray-light bg-white">
-			<Link
-				href={NavItems.home}
-				className="m-4 flex w-1/6 justify-center text-nowrap text-gray"
-			>
-				Tech Blog_Jasmine
-			</Link>
-			<nav>
-				<ul className="flex flex-row">
-					{Object.entries(NavItems).map(([label, href]) => (
-						<li key={label} className="m-6 text-gray">
-							<Link href={href as Route}>{label}</Link>
-						</li>
-					))}
-				</ul>
-			</nav>
-		</header>
+		<div className="fixed top-0 border-b border-gray-light">
+			<header className="flex h-16 w-screen flex-row items-center justify-around border border-gray-light bg-white ">
+				<Link
+					href={NavItems.home}
+					className="m-4 flex w-1/6 justify-center text-nowrap text-gray"
+				>
+					Tech Blog_Jasmine
+				</Link>
+				<nav>
+					<ul className="flex flex-row">
+						{Object.entries(NavItems).map(([label, href]) => (
+							<li key={label} className="m-6 text-gray">
+								<Link href={href as Route}>{label}</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
+			</header>
+		</div>
 	);
 };
 
