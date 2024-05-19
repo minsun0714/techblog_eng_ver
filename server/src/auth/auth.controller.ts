8ac9d832fca72d5cecb36552fa3b8ce4f0e6a8be
jasmine0714 <woooooooow22@gmail.com>
2024-05-19 13:47:05 +0900

    ✨ 쿠키에 access token 발급

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,5 +1,13 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -7,7 +15,15 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  signIn(@Body() signInDto) {
-    return this.authService.signIn(signInDto.username, signInDto.password);
+  signIn(@Body() signInDto, @Res() res: Response) {
+    const accessToken = this.authService.signIn(
+      signInDto.username,
+      signInDto.password,
+    );
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+    return res.send({ message: 'Login successful' });
   }
 }
