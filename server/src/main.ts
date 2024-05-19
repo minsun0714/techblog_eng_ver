298d7bdc51915152ffdd88ea33600195b681e42d
jasmine0714 <woooooooow22@gmail.com>
2024-05-19 22:19:57 +0900

    🔧 클라이언트에서 Authorization 에 접근가능하도록 Cors 설정 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true, // 쿠키 전달을 허용
+    exposedHeaders: ['Authorization'],
+  });
+  app.use(cookieParser());
   await app.listen(8000);
 }
 bootstrap();
