456a1cf2efcdcb2e67fb1d10a71b48408c283ad7
jasmine0714 <woooooooow22@gmail.com>
2024-05-19 22:20:24 +0900

    ✨ token을 store에 저장

--- client/src/service/useHandleLogin.ts
+++ client/src/service/useHandleLogin.ts
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/navigation';
+import useAuthStore from '@/app/(common)/store/useAuthStore';
+
 const useHandleLogin = () => {
+	const router = useRouter();
+	const { setToken } = useAuthStore();
+
 	const handleLogin = async (formData: FormData) => {
 		const authInfo = {
 			username: formData.get('username'),
@@ -12,9 +18,18 @@ const useHandleLogin = () => {
 					'Content-Type': 'application/json',
 				},
 				body: JSON.stringify(authInfo),
+				credentials: 'include', // 쿠키 포함
 			});
+
 			if (!response.ok) {
 				throw new Error();
+			}
+
+			const accessToken = response.headers.get('Authorization')?.split(' ')[1];
+
+			if (accessToken) {
+				setToken(accessToken);
+				router.push('/admin');
 			}
 		} catch (err) {
 			alert('아이디 또는 비밀번호가 일치하지 않습니다.');
