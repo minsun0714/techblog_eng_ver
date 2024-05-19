1fcb3a7806cb9c0a08f0997f38908315a3e953cc
jasmine0714 <woooooooow22@gmail.com>
2024-05-19 22:42:49 +0900

    ✨ token이 없는 경우 login 페이지로 redirect

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import Button from '../(common)/components/Button';
+import useAuthStore from '../(common)/store/useAuthStore';
 import Form from './components/Form';
 import Input from './components/Input';
 import Select from './components/Select';
 import UpDownBtn from './components/UpDownBtn';
-import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 export type FormElements = {
@@ -17,6 +19,15 @@ export type FormElements = {
 const AdminPage = () => {
 	const [isOpen, setIsOpen] = useState<boolean>(true);
 	const [value, setValue] = useState<string | undefined>('');
+
+	const router = useRouter();
+	const { token } = useAuthStore();
+
+	useEffect(() => {
+		if (!token) {
+			router.push('/login');
+		}
+	}, [token, router]);
 
 	return (
 		<Form>
