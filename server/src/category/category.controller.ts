ef3480177607acf8114e5cf0d3ae3dffedfba04a
jasmine0714 <woooooooow22@gmail.com>
2024-05-20 18:23:23 +0900

    ✨ category API 생성

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -7,8 +7,16 @@ export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
   @Get()
-  async category(@Res() res: Response) {
+  async getCategory(@Res() res: Response) {
     const category = await this.categoryService.getCategory();
+    return res
+      .status(200)
+      .json({ message: 'category를 성공적으로 받았습니다.', category });
+  }
+
+  @Get('2')
+  async getCategoryChildren(@Res() res: Response) {
+    const category = await this.categoryService.getCategoryChildren();
     return res
       .status(200)
       .json({ message: 'category를 성공적으로 받았습니다.', category });
--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -9,36 +9,46 @@ type Children = {
   children: Category[];
 };
 
+const categoryList = [
+  {
+    id: 0,
+    title: 'Algorithm',
+    children: [
+      {
+        id: 0,
+        title: 'Leetcode',
+      },
+      {
+        id: 1,
+        title: 'Baekjoon',
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: 'Project',
+    children: [
+      {
+        id: 0,
+        title: 'building my own tech blog',
+      },
+    ],
+  },
+];
+
 @Injectable()
 export class CategoryService {
   constructor() {}
 
   async getCategory(): Promise<(Category & Children)[]> {
-    return [
-      {
-        id: 0,
-        title: 'Algorithm',
-        children: [
-          {
-            id: 0,
-            title: 'Leetcode',
-          },
-          {
-            id: 1,
-            title: 'Baekjoon',
-          },
-        ],
-      },
-      {
-        id: 1,
-        title: 'Project',
-        children: [
-          {
-            id: 0,
-            title: 'building my own tech blog',
-          },
-        ],
-      },
-    ];
+    return categoryList;
+  }
+
+  async getCategoryChildren(): Promise<Category[][]> {
+    const children = categoryList.map(
+      (category: Category & Children) => category.children,
+    );
+
+    return children;
   }
 }
