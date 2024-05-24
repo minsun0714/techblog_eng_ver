49090d861c1ce19906d1f7e9b79d4142ab940a8e
jasmine0714 <woooooooow22@gmail.com>
2024-05-24 18:51:30 +0900

    🗃️ 메모리에 저장했던 카테고리 데이터를 DB에 저장

--- server/src/category/category.entity.ts
+++ server/src/category/category.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
 
 @Entity({ name: 'category' }) // 생성될 테이블 이름
 export class Category {
@@ -7,4 +14,16 @@ export class Category {
 
   @Column()
   title: string;
+  // 부모 엔티티가 여러 자식 엔티티를 참조
+
+  @OneToMany(() => Category, (category) => category.parent)
+  children: Category[];
+
+  // 자식 엔티티가 부모 엔티티를 참조
+  @ManyToOne(() => Category, (category) => category.children) // 부모의 children 필드로 자식에 접근 가능
+  @JoinColumn({ name: 'parentId' })
+  parent: Category | null;
+
+  @Column({ nullable: true })
+  parentId: number | null;
 }
--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -3,37 +3,43 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Category } from './category.entity';
 
-// type CategoryType = {
-//   id: number;
-//   title: string;
-// };
-
-// type Children = {
-//   children: Category[];
-// };
+interface CategoryData {
+  id: number;
+  title: string;
+  parentId: number | null;
+  children: CategoryData[];
+}
 
 const categoryList = [
   {
     id: 0,
     title: 'Algorithm',
+    parentId: null,
     children: [
       {
         id: 1,
         title: 'Leetcode',
+        parentId: 0,
+        children: [],
       },
       {
         id: 2,
         title: 'Baekjoon',
+        parentId: 0,
+        children: [],
       },
     ],
   },
   {
     id: 3,
     title: 'Project',
+    parentId: null,
     children: [
       {
         id: 4,
         title: 'building my own tech blog',
+        parentId: 3,
+        children: [],
       },
     ],
   },
@@ -50,25 +56,39 @@ export class CategoryService {
     return this.categoryRepository.find();
   }
 
-  // async getCategoryChildren(id: number): Promise<Category[]> {
-  //   const children = categoryList.filter(
-  //     (category: Category & Children) => category.id === id,
-  //   )[0].children;
+  async getCategoryChildren(id: number): Promise<Category[]> {
+    const parentCategory = await this.categoryRepository.findOne({
+      where: { id },
+      relations: ['children'],
+    });
 
-  //   return children;
-  // }
+    if (!parentCategory) {
+      return [];
+    }
+    console.log(parentCategory);
+    return parentCategory.children;
+  }
 
   async saveCategories(): Promise<void> {
     for (const categoryData of categoryList) {
-      await this.saveCategory(categoryData);
+      await this.saveCategoryAndChildren(categoryData, null);
     }
   }
 
-  private async saveCategory(categoryData: Category): Promise<void> {
+  private async saveCategoryAndChildren(
+    categoryData: CategoryData,
+    parentId: number | null,
+  ): Promise<void> {
     const category = new Category();
-    category.id = categoryData.id;
     category.title = categoryData.title;
+    category.parentId = parentId;
 
-    await this.categoryRepository.save(category);
+    const savedCategory = await this.categoryRepository.save(category); // id를 자동으로 부여받음
+
+    if (categoryData.children && categoryData.children.length > 0) {
+      for (const childData of categoryData.children) {
+        await this.saveCategoryAndChildren(childData, savedCategory.id); // 자동으로 부여받은 id를 자식을 호출하며 parentId로 지정하라고 넘겨줌
+      }
+    }
   }
 }
