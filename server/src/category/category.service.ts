d94437e75f014384bd64cdad3f30794639d68322
jasmine0714 <woooooooow22@gmail.com>
2024-05-24 19:54:59 +0900

    🔥 메모리에 저장한 데이터를 DB로 옮기는 코드 삭제

--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -3,48 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Category } from './category.entity';
 
-interface CategoryData {
-  id: number;
-  title: string;
-  parentId: number | null;
-  children: CategoryData[];
-}
-
-const categoryList = [
-  {
-    id: 0,
-    title: 'Algorithm',
-    parentId: null,
-    children: [
-      {
-        id: 1,
-        title: 'Leetcode',
-        parentId: 0,
-        children: [],
-      },
-      {
-        id: 2,
-        title: 'Baekjoon',
-        parentId: 0,
-        children: [],
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: 'Project',
-    parentId: null,
-    children: [
-      {
-        id: 4,
-        title: 'building my own tech blog',
-        parentId: 3,
-        children: [],
-      },
-    ],
-  },
-];
-
 @Injectable()
 export class CategoryService {
   constructor(
@@ -53,7 +11,8 @@ export class CategoryService {
   ) {}
 
   async getCategory(): Promise<Category[]> {
-    return this.categoryRepository.find();
+    const categories = await this.categoryRepository.find();
+    return categories.filter((category) => category.parentId === null);
   }
 
   async getCategoryChildren(id: number): Promise<Category[]> {
@@ -65,30 +24,6 @@ export class CategoryService {
     if (!parentCategory) {
       return [];
     }
-    console.log(parentCategory);
     return parentCategory.children;
-  }
-
-  async saveCategories(): Promise<void> {
-    for (const categoryData of categoryList) {
-      await this.saveCategoryAndChildren(categoryData, null);
-    }
-  }
-
-  private async saveCategoryAndChildren(
-    categoryData: CategoryData,
-    parentId: number | null,
-  ): Promise<void> {
-    const category = new Category();
-    category.title = categoryData.title;
-    category.parentId = parentId;
-
-    const savedCategory = await this.categoryRepository.save(category); // id를 자동으로 부여받음
-
-    if (categoryData.children && categoryData.children.length > 0) {
-      for (const childData of categoryData.children) {
-        await this.saveCategoryAndChildren(childData, savedCategory.id); // 자동으로 부여받은 id를 자식을 호출하며 parentId로 지정하라고 넘겨줌
-      }
-    }
   }
 }
