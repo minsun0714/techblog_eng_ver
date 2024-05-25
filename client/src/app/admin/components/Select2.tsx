0739ad2addfccb956c5e3f6bb18e9f3ef5cd88bc
jasmine0714 <woooooooow22@gmail.com>
2024-05-25 15:53:49 +0900

    🥅 options를 비동기적으로 fetch하기 전에 style을 먼저 parsing해서 뜨는 에러 해결

--- client/src/app/admin/components/Select2.tsx
+++ client/src/app/admin/components/Select2.tsx
@@ -21,7 +21,7 @@ const Select2 = ({ name, options }: SelectType) => {
 		<li className="flex w-full flex-col">
 			<label className="text-gray-mid-light">{name}</label>
 			<select
-				className={`h-10 rounded-sm border border-gray-mid-light focus:outline-gray ${options.length ? 'bg-white' : 'bg-gray-light'} `}
+				className={`h-10 rounded-sm border border-gray-mid-light focus:outline-gray ${options && options.length ? 'bg-white' : 'bg-gray-light'} `}
 			>
 				{options?.map((option) => (
 					<option key={option.id} value={categoryId > -1 ? option.id : ''}>
