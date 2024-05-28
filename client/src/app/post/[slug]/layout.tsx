33d6f8ed9b9eaea5553234f3d438ba1728a43cf5
jasmine0714 <woooooooow22@gmail.com>
2024-05-28 10:47:24 +0900

    💄 title이 중간으로 오도록 스타일 변경

--- client/src/app/post/[slug]/layout.tsx
+++ client/src/app/post/[slug]/layout.tsx
@@ -1,7 +1,9 @@
 const PostLayout = async ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="flex h-screen w-full items-start justify-center py-24">
-			<main className="flex w-2/3 lg:w-[900px]">{children}</main>
+		<div className="flex h-screen w-screen items-start justify-center py-24">
+			<main className="flex w-2/3 items-center justify-center lg:w-[900px]">
+				{children}
+			</main>
 		</div>
 	);
 };
--- client/src/app/post/[slug]/page.tsx
+++ client/src/app/post/[slug]/page.tsx
@@ -4,7 +4,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
 	const article = await getArticleById(params.slug);
 	return (
 		<main className="flex flex-col items-center justify-center gap-y-20">
-			<h1 className="text-5xl">{article.title}</h1>
+			<h1 className="text-center text-5xl">{article.title}</h1>
 			<p>{article.content}</p>
 		</main>
 	);
