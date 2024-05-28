a6289402a54d61ce28c0b1d362854a07406c2ac2
jasmine0714 <woooooooow22@gmail.com>
2024-05-28 11:28:18 +0900

    ✨ form 제출 시 posts 페이지로 redirect

--- client/src/app/admin/components/Form.tsx
+++ client/src/app/admin/components/Form.tsx
@@ -2,6 +2,7 @@ import { usePostArticle } from '@/service/usePostArticles';
 
 const Form = ({ children }: { children: React.ReactNode }) => {
 	const handleSubmit = usePostArticle();
+
 	return (
 		<form className="flex w-2/3 flex-col gap-y-8 p-20" onSubmit={handleSubmit}>
 			{children}
--- client/src/service/usePostArticles.ts
+++ client/src/service/usePostArticles.ts
@@ -1,11 +1,14 @@
+import useSWRMutation from 'swr/mutation';
+import { Route } from 'next';
+import { useRouter } from 'next/navigation';
 import usePostingFieldStore from '@/app/admin/store/usePostingFieldStore';
 
 export const usePostArticle = () => {
 	const { title, categoryId, categoryChildId, content } =
 		usePostingFieldStore();
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const response = await fetch('http://localhost:8000/articles', {
+	const router = useRouter();
+	const postArticle = async () => {
+		await fetch('http://localhost:8000/articles', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
@@ -17,7 +20,15 @@ export const usePostArticle = () => {
 				content,
 			}),
 		});
-		return await response.json();
+	};
+	const options = {
+		onSuccess: () => router.push('/posts' as Route),
+	};
+	const { trigger } = useSWRMutation('articles', postArticle, options);
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		await trigger();
 	};
 	return handleSubmit;
 };
